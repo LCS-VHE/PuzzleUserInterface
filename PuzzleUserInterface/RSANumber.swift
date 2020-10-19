8048dc86eb5d09eca4cd7ec55b7d07f5549d790e
Vincent He <vincent0710@Outlook.com>
2020-10-19 11:58:40 -0400

    Made a function in the RSA Calculator called divisorCount

--- PuzzleUserInterface/RSANumber.swift
+++ PuzzleUserInterface/RSANumber.swift
@@ -7,12 +7,46 @@
 
 import SwiftUI
 
+struct RSAcalculator{
+    func numberOfRSA() -> String{
+        return "ERROR"
+    }
+    
+    func divisorCount(of givenNumber: Int) -> Int {
+        
+        // 1 is always a divisor
+        var divisorCount = 1
+        
+        // Start looping from 2
+        for i in 2...givenNumber {
+            
+            // What is the remainder?
+            let remainder = givenNumber % i
+            
+            // DEBUG output...
+            //        print("\(givenNumber) \\ \(i) has a remainder of \(remainder)")
+            
+            // TODO: Add some code here... we need to keep track of how many divisors there are
+            if remainder == 0 {
+                divisorCount += 1
+            }
+            
+        }
+        
+        // Return the count of divisors
+        return divisorCount
+        
+    }
+}
+
 struct RSANumber: View {
     var body: some View {
-        VStack{
-            Text("Hello World")
-        }
-    }
+        NavigationView{
+            VStack{
+                
+                Text("Hello World")
+            }
+        }}
 }
 
 struct RSANumber_Previews: PreviewProvider {
